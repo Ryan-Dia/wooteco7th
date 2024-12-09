f2ab320722f2cff2d612e336f3ef35330801f63c
ryan-dia <cjfdnjs158@naver.com>
2024-12-09 22:26:33 +0900

    feat: 다리 길이 입력 받기

--- 5-period/java-bridge-main/src/main/java/bridge/InputView.java
+++ 5-period/java-bridge-main/src/main/java/bridge/InputView.java
@@ -1,15 +1,23 @@
 package bridge;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
-public class InputView {
+public final class InputView {
+    private InputView() {}
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public static int readBridgeSize() {
+        try {
+            System.out.println("다리의 길이를 입력해주세요.");
+            return Integer.parseInt(readLine());
+        }catch (IllegalArgumentException o) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
+        }
     }
 
     /**
--- 5-period/java-bridge-main/src/main/java/bridge/controller/BridgeController.java
+++ 5-period/java-bridge-main/src/main/java/bridge/controller/BridgeController.java
@@ -1,10 +1,12 @@
 package bridge.controller;
 
+import bridge.InputView;
 import bridge.OutputView;
 
 public class BridgeController {
 
     public void run() {
         OutputView.printStartMessage();
+        InputView.readBridgeSize();
     }
 }
