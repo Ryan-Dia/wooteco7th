b3308f4eddbd7359b20d3a86f15908a39d05456d
ryan-dia <cjfdnjs158@naver.com>
2024-12-12 13:58:59 +0900

    fix: 코치 이름 중복 불가

--- 5-period/java-menu-main/src/main/java/menu/model/coach/Coach.java
+++ 5-period/java-menu-main/src/main/java/menu/model/coach/Coach.java
@@ -1,5 +1,6 @@
 package menu.model.coach;
 
+import java.util.Objects;
 import menu.error.CIllegalArgumentException;
 import menu.error.ErrorMessages;
 
@@ -8,6 +9,7 @@ public class Coach {
     private static final int NAME_LENGTH_MAX = 4;
     private static final String REGEX = "^[가-힣]+";
     private final String name;
+    private Inedible inedible;
 
     public Coach(final String name) {
         validateNameLength(name);
@@ -27,7 +29,28 @@ public class Coach {
         }
     }
 
+    public void setInedible(Inedible inedible) {
+        this.inedible = inedible;
+    }
+
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Coach coach = (Coach) o;
+        return Objects.equals(name, coach.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
--- 5-period/java-menu-main/src/main/java/menu/model/coach/Coaches.java
+++ 5-period/java-menu-main/src/main/java/menu/model/coach/Coaches.java
@@ -6,10 +6,23 @@ public class Coaches {
     private final List<Coach> coaches;
 
     public Coaches(final List<Coach> coaches) {
+        validateDuplication(coaches);
         this.coaches = coaches;
+    }
+
+    //TODO: List 중복검사 템플릿 만들기
+    public void validateDuplication(List<Coach> coaches) {
+        final long count = coaches.stream().distinct().count();
+        if (count != coaches.size()) {
+            throw new IllegalArgumentException("[ERROR] 코치이름은 중복을 허용하지 않습니다.");
+        }
     }
 
     public List<Coach> getCoaches() {
         return coaches;
+    }
+
+    public List<String> getAllCoachName() {
+        return coaches.stream().map(Coach::getName).toList();
     }
 }
