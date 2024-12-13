635cbc0fa03b378247ddc8f51f11084f3110eee2
ryan-dia <cjfdnjs158@naver.com>
2024-12-13 13:41:33 +0900

    fix: 최대 전체 메뉴 주문 개수는 20개로 제한

--- 6-period/java-christmas-6/src/main/java/christmas/model/order/Orders.java
+++ 6-period/java-christmas-6/src/main/java/christmas/model/order/Orders.java
@@ -6,12 +6,23 @@ import java.util.HashSet;
 import java.util.List;
 
 public class Orders {
+    public static final int MAX_ORDERS_COUNT = 20;
     public final List<Order> orders;
 
     public Orders(final List<Order> orders) {
         validateOrderDuplication(orders);
         validateOnlyBeverage(orders);
+        validateCount(orders);
         this.orders = orders;
+    }
+
+    private void validateCount(List<Order> orders) {
+        final int sum = orders.stream()
+                .mapToInt(Order::getQuantity)
+                .sum();
+        if (sum > MAX_ORDERS_COUNT) {
+            throw new CIllegalArgumentException(ErrorMessages.INVALID_ORDER);
+        }
     }
 
     private void validateOrderDuplication(List<Order> orders) {
